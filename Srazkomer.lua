3f52d4481d9f26d57fd37ab3eb81a102816aa48b
pfory <pfory@seznam.cz>
2016-07-04 19:16:16 +0200

    v0,5 lua 151 4.7.2016

--- Srazkomer.lua
+++ Srazkomer.lua
@@ -8,23 +8,13 @@ heartBeat = node.bootreason() + 10
 uart.write(0, "Boot reason:")
 print(heartBeat)
 
-wifi.setmode(wifi.STATION)
-wifi.sta.config("Datlovo","Nu6kMABmseYwbCoJ7LyG")
-cfg={
-  ip = "192.168.1.150",
-  netmask = "255.255.255.0",
-  gateway = "192.168.1.1"
-}
-wifi.sta.setip(cfg)
-wifi.sta.autoconnect(1)
-
 Broker="88.146.202.186"  
 
 pinLed = 4
 gpio.mode(pinLed,gpio.OUTPUT)  
 gpio.write(pinLed,gpio.HIGH)  
 
-versionSW         = 0.4
+versionSW         = 0.5
 versionSWString   = "Srazkomer v" 
 print(versionSWString .. versionSW)
 
@@ -114,34 +104,25 @@ m:on("message", function(conn, topic, data)
 end)  
 
 
-uart.write(0,"Connecting to Wifi")
-tmr.alarm(0, 1000, 1, function() 
-  uart.write(0,".")
-  if wifi.sta.status() == 5 and wifi.sta.getip() ~= nil then 
-    print ("Wifi connected")
-    print(wifi.sta.getmac())
-    tmr.stop(0) 
-    m:connect(Broker, 31883, 0, 1, function(conn) 
-      mqtt_sub() --run the subscription function 
-      print(wifi.sta.getip())
-      print("Mqtt Connected to:" .. Broker.." - "..base) 
-      gpio.trig(pin, "both", pinPulse)
-      uart.write(0,"Read config file... ")
-      file.open("config.ini", "r")
-      s = file.readline()
-      if (s==nil) then
-        uart.write(0,"empty file.")
-        pulseTotal=0
-      else
-        pulseTotal = tonumber(s)
-      end
-      uart.write(0,pulseTotal.." pulse(s).\n\r")
-      file.close()  
-      m:publish(base.."VersionSW",   versionSW,0,0)  
-      sendHB() 
-      tmr.alarm(0, 60000, tmr.ALARM_AUTO, function()
-        sendHB() 
-      end)
-    end) 
+m:connect(Broker, 31883, 0, 1, function(conn) 
+  mqtt_sub() --run the subscription function 
+  print(wifi.sta.getip())
+  print("Mqtt Connected to:" .. Broker.." - "..base) 
+  gpio.trig(pin, "both", pinPulse)
+  uart.write(0,"Read config file... ")
+  file.open("config.ini", "r")
+  s = file.readline()
+  if (s==nil) then
+    uart.write(0,"empty file.")
+    pulseTotal=0
+  else
+    pulseTotal = tonumber(s)
   end
-end)
+  uart.write(0,pulseTotal.." pulse(s).\n\r")
+  file.close()  
+  m:publish(base.."VersionSW",   versionSW,0,0)  
+  sendHB() 
+  tmr.alarm(0, 60000, tmr.ALARM_AUTO, function()
+    sendHB() 
+  end)
+end) 
