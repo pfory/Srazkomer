48116420508cff2225615281c5f05cd128794f69
Petr <pfory@seznam.cz>
2020-07-23 11:54:23 +0200

    ota

--- ota.sh
+++ ota.sh
@@ -2,4 +2,4 @@
 upravit verzi XXXX
 spustit v příkazové řádce
 
-sudo python /root/.arduino15/packages/esp8266/hardware/esp8266/2.4.2/tools/espota.py -i 192.168.1.107 -p 8266 --auth=password -f /home/pi/Srazkomer.ino.d1_mini.bin
+sudo python /root/.arduino15/packages/esp8266/hardware/esp8266/2.4.2/tools/espota.py -i 192.168.1.107 -p 8266 --auth=password -f /home/petr/Srazkomer.ino.d1_mini.bin
