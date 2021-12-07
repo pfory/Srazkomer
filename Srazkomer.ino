b97153220d56f756a976e420c032644db0d3b639
pfory <pfory@seznam.cz>
2021-12-07 21:13:55 +0100

    1.70

--- Srazkomer.ino
+++ Srazkomer.ino
@@ -12,12 +12,6 @@ ESP8266WebServer server(80);
 unsigned int volatile pulseCount            = 0;
 unsigned long lastPulseMillis               = 0;
 
-uint32_t heartBeat                          = 0;
-
-DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
-
-WiFiClient espClient;
-PubSubClient client(espClient);
 
 void ICACHE_RAM_ATTR pulseCountEvent() {
   digitalWrite(BUILTIN_LED, LOW);
@@ -29,19 +23,6 @@ void ICACHE_RAM_ATTR pulseCountEvent() {
 }
 
 ADC_MODE(ADC_VCC);
-
-bool isDebugEnabled() {
-#ifdef verbose
-  return true;
-#endif // verbose
-  return false;
-}
-
-//for LED status
-Ticker ticker;
-
-auto timer = timer_create_default(); // create a timer with default settings
-Timer<> default_timer; // save as above
 
 //MQTT callback
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -78,129 +59,26 @@ void handleRoot() {
 }
 #endif
 
-void tick() {
-  //toggle state
-  int state = digitalRead(BUILTIN_LED);  // get the current state of GPIO1 pin
-  digitalWrite(BUILTIN_LED, !state);     // set pin to the opposite state
-}
-  
-//gets called when WiFiManager enters configuration mode
-void configModeCallback (WiFiManager *myWiFiManager) {
-  DEBUG_PRINTLN("Entered config mode");
-  DEBUG_PRINTLN(WiFi.softAPIP());
-  //if you used auto generated SSID, print it
-  DEBUG_PRINTLN(myWiFiManager->getConfigPortalSSID());
-}
-
-WiFiManager wifiManager;
-
 void setup() {
-  SERIAL_BEGIN;
-  DEBUG_PRINT(F(SW_NAME));
-  DEBUG_PRINT(F(" "));
-  DEBUG_PRINTLN(F(VERSION));
+  preSetup();
   
-  pinMode(STATUS_LED, OUTPUT);
-  ticker.attach(1, tick);
-    
-  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
-
-  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-  wifiManager.setAPCallback(configModeCallback);
-  wifiManager.setConfigPortalTimeout(CONFIG_PORTAL_TIMEOUT);
-  wifiManager.setConnectTimeout(CONNECT_TIMEOUT);
-  wifiManager.setBreakAfterConfig(true);
-  wifiManager.setWiFiAutoReconnect(true);
-  
-  if (drd.detectDoubleReset()) {
-    drd.stop();
-    DEBUG_PRINTLN("Double reset detected, starting config portal...");
-    if (!wifiManager.startConfigPortal(HOSTNAMEOTA)) {
-      DEBUG_PRINTLN("Failed to connect. Use ESP without WiFi.");
-    }
-  }
- 
-  rst_info *_reset_info = ESP.getResetInfoPtr();
-  uint8_t _reset_reason = _reset_info->reason;
-  DEBUG_PRINT("Boot-Mode: ");
-  DEBUG_PRINTLN(_reset_reason);
-  heartBeat = _reset_reason;
-  /*
-  REASON_DEFAULT_RST             = 0      normal startup by power on 
-  REASON_WDT_RST                 = 1      hardware watch dog reset 
-  REASON_EXCEPTION_RST           = 2      exception reset, GPIO status won't change 
-  REASON_SOFT_WDT_RST            = 3      software watch dog reset, GPIO status won't change 
-  REASON_SOFT_RESTART            = 4      software restart ,system_restart , GPIO status won't change 
-  REASON_DEEP_SLEEP_AWAKE        = 5      wake up from deep-sleep 
-  REASON_EXT_SYS_RST             = 6      external system reset 
-  */
-  
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
-
-  if (!wifiManager.autoConnect(AUTOCONNECTNAME, AUTOCONNECTPWD)) { 
-    DEBUG_PRINTLN("Autoconnect failed connect to WiFi. Use ESP without WiFi.");
-  }   
-
-  WiFi.printDiag(Serial);
-  
-  sendNetInfoMQTT();
-
-#ifdef serverHTTP
-  server.on ( "/", handleRoot );
-  server.begin();
-  DEBUG_PRINTLN ( "HTTP server started!!" );
-#endif
-
-#ifdef time
-  DEBUG_PRINTLN("Setup TIME");
-  EthernetUdp.begin(localPort);
-  DEBUG_PRINT("Local port: ");
-  DEBUG_PRINTLN(EthernetUdp.localPort());
-  DEBUG_PRINTLN("waiting for sync");
-  setSyncProvider(getNtpTime);
-  setSyncInterval(300);
-  
-  printSystemTime();
-#endif
-
-#ifdef ota
-  ArduinoOTA.setHostname(HOSTNAMEOTA);
-
-  ArduinoOTA.onStart([]() {
-    DEBUG_PRINTLN("Start updating ");
-  });
-  ArduinoOTA.onEnd([]() {
-   DEBUG_PRINTLN("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    DEBUG_PRINTF("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    DEBUG_PRINTF("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) DEBUG_PRINTLN("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) DEBUG_PRINTLN("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) DEBUG_PRINTLN("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) DEBUG_PRINTLN("Receive Failed");
-    else if (error == OTA_END_ERROR) DEBUG_PRINTLN("End Failed");
-  });
-  ArduinoOTA.begin();
-#endif
- 
   //v klidu LOW, pulz HIGH
   /*It have the output state light, if output high level, the lights are off. if output low level ,it on.
   If it covered,it will output high level;otherwise it output low level.*/
   pinMode(INTERRUPTPIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(INTERRUPTPIN), pulseCountEvent, RISING);
-  
+
+#ifdef timers  
   //setup timers
   timer.every(SENDSTAT_DELAY, sendStatisticMQTT);
   timer.every(CONNECT_DELAY, reconnect);
+#endif
 
   void * a;
-  sendStatisticMQTT(a);
   reconnect(a);
- 
+  sendStatisticMQTT(a);
+  sendNetInfoMQTT();
+  
   ticker.detach();
   //keep LED on
   digitalWrite(BUILTIN_LED, HIGH);
@@ -231,96 +109,27 @@ void loop() {
 #endif
 }
 
-void startConfigPortal(void) {
-  DEBUG_PRINTLN("START config portal");
-  wifiManager.setConfigPortalBlocking(false);
-  wifiManager.startConfigPortal(HOSTNAMEOTA);
-}
-
-void stopConfigPortal(void) {
-  DEBUG_PRINTLN("STOP config portal");
-  wifiManager.stopConfigPortal();
-}
-
 bool sendDataMQTT() {
   digitalWrite(BUILTIN_LED, LOW);
   DEBUG_PRINTLN(F("Data"));
   
-  SenderClass sender;
-  
-  sender.add("Pulse", pulseCount );
-
+  client.publish((String(mqtt_base) + "/Pulse").c_str(), String(pulseCount).c_str());
+ 
   DEBUG_PRINTLN(F("Calling MQTT"));
 
-  if (sender.sendMQTT(mqtt_server, mqtt_port, mqtt_username, mqtt_key, mqtt_base)) {
-      pulseCount = 0;
-  }
-  
+  pulseCount = 0;
+ 
   digitalWrite(BUILTIN_LED, HIGH);
   return true;
 }
-
-bool sendStatisticMQTT(void *) {
-  digitalWrite(BUILTIN_LED, LOW);
-  DEBUG_PRINTLN(F("Statistic"));
-
-  SenderClass sender;
-  sender.add("VersionSW", VERSION);
-  sender.add("Napeti",  ESP.getVcc());
-  sender.add("HeartBeat", heartBeat++);
-  sender.add("RSSI", WiFi.RSSI());
-  DEBUG_PRINTLN(F("Calling MQTT"));
-  
-  sender.sendMQTT(mqtt_server, mqtt_port, mqtt_username, mqtt_key, mqtt_base);
-  digitalWrite(BUILTIN_LED, HIGH);
-  return true;
-}
-
-void sendNetInfoMQTT() {
-  digitalWrite(BUILTIN_LED, LOW);
-  DEBUG_PRINTLN(F("Net info"));
-
-  SenderClass sender;
-  sender.add("IP",              WiFi.localIP().toString().c_str());
-  sender.add("MAC",             WiFi.macAddress());
-  
-  DEBUG_PRINTLN(F("Calling MQTT"));
-  
-  sender.sendMQTT(mqtt_server, mqtt_port, mqtt_username, mqtt_key, mqtt_base);
-  digitalWrite(BUILTIN_LED, HIGH);
-  return;
-}
-
-
-void printDigits(int digits){
-  // utility function for digital clock display: prints preceding
-  // colon and leading 0
-  DEBUG_PRINT(":");
-  if(digits < 10)
-    DEBUG_PRINT('0');
-  DEBUG_PRINT(digits);
-}
-
-// void printSystemTime(){
-// #ifdef time
-  // DEBUG_PRINT(day());
-  // DEBUG_PRINT(".");
-  // DEBUG_PRINT(month());
-  // DEBUG_PRINT(".");
-  // DEBUG_PRINT(year());
-  // DEBUG_PRINT(" ");
-  // DEBUG_PRINT(hour());
-  // printDigits(minute());
-  // printDigits(second());
-// #endif
-// }
 
 bool reconnect(void *) {
   if (!client.connected()) {
     DEBUG_PRINT("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect(mqtt_base, mqtt_username, mqtt_key)) {
+     if (client.connect(mqtt_base, mqtt_username, mqtt_key, (String(mqtt_base) + "/LWT").c_str(), 2, true, "Dead", false)) {
       client.subscribe((String(mqtt_base) + "/#").c_str());
+      client.publish((String(mqtt_base) + "/connected").c_str(), "");
       DEBUG_PRINTLN("connected");
     } else {
       DEBUG_PRINT("failed, rc=");
--- configuration.h
+++ configuration.h
@@ -7,54 +7,23 @@
 #include <Ticker.h>
 #include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
 #include "DoubleResetDetector.h" // https://github.com/datacute/DoubleResetDetector
-#include "Sender.h"
 #include <Wire.h>
 #include <timer.h>
+#include <PubSubClient.h>
 
 //SW name & version
-#define     VERSION                          "1.60"
+#define     VERSION                          "1.70"
 #define     SW_NAME                          "Srazkomer"
-
-//HW
-//Wemos D1 R2 & mini 
 
 #define timers
 #define ota
 #define verbose
 //#define time
 //#define serverHTTP
-#ifdef serverHTTP
-#include <ESP8266WebServer.h>
-#endif
 
-#define AUTOCONNECTNAME   HOSTNAMEOTA
-#define AUTOCONNECTPWD    "password"
-
-#ifdef ota
-#include <ArduinoOTA.h>
-#define HOSTNAMEOTA   SW_NAME VERSION
-#endif
-
-#ifdef verbose
-  #define DEBUG_PRINT(x)                     Serial.print (x)
-  #define DEBUG_PRINTDEC(x)                  Serial.print (x, DEC)
-  #define DEBUG_PRINTLN(x)                   Serial.println (x)
-  #define DEBUG_PRINTF(x, y)                 Serial.printf (x, y)
-  #define PORTSPEED 115200             
-  #define DEBUG_WRITE(x)                     Serial.write (x)
-  #define DEBUG_PRINTHEX(x)                  Serial.print (x, HEX)
-  #define SERIAL_BEGIN                       Serial.begin(PORTSPEED)
-#else
-  #define DEBUG_PRINT(x)
-  #define DEBUG_PRINTDEC(x)
-  #define DEBUG_PRINTLN(x)
-  #define DEBUG_PRINTF(x, y)
-  #define DEBUG_WRITE(x)
-#endif 
 
 
 //All of the IO pins have interrupt/pwm/I2C/one-wire support except D0.
-#define STATUS_LED                          BUILTIN_LED //status LED
 //#define                                                   10k Pull-up     GPIO00
 //#define                                                                   GPIO16
 //#define                                                   MISO            GPIO12
@@ -76,16 +45,12 @@ static const char* const      mqtt_config_portal             = "config";
 static const char* const      mqtt_config_portal_stop        = "disconfig";
 
 
-// Number of seconds after reset during which a
-// subseqent reset will be considered a double reset.
-#define DRD_TIMEOUT 2
-// RTC Memory Address for the DoubleResetDetector to use
-#define DRD_ADDRESS 0
-
 #define CONFIG_PORTAL_TIMEOUT 60 //jak dlouho zustane v rezimu AP nez se cip resetuje
 #define CONNECT_TIMEOUT 5 //jak dlouho se ceka na spojeni nez se aktivuje config portal
   
 #define SENDSTAT_DELAY                       60000 //poslani statistiky kazdou minutu
 #define CONNECT_DELAY                        5000 //ms
+
+#include <fce.h>
 
 #endif
